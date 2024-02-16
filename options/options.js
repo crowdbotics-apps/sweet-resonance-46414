b3d29e9854566d513afc568a7a6c9143f67339b6
Crowdbotics <team@crowdbotics.com>
2024-02-16 18:24:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sweet-resonance-46414",
+  url: "https://sweet-resonance-46414.botics.co",
+  api: "https://sweet-resonance-46414.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
